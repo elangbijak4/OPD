933bc382a60f97724651fe9117d3c7c3a5a5e250
Aslan Alwi <elangbijak4@gmail.com>
2020-08-22 12:47:55 +0700

    ini adalah proyek landing page OPD

---
+++ app/Views/wonuaku/wonuaku.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>e-Sinra BPSDM Prov. Sulawesi Selatan</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?php echo base_url('/assets/images/favicon.png')?>" rel="icon">
+  <link href="<?php echo base_url('/assets/images/apple-touch-icon.png')?>" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+  <link href="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/icofont/icofont.min.css')?>" rel="stylesheet">
+  <link href="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/remixicon/remixicon.css')?>" rel="stylesheet">
+  <link href="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/boxicons/css/boxicons.min.css')?>" rel="stylesheet">
+  <link href="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/owl.carousel/assets/owl.carousel.min.css')?>" rel="stylesheet">
+  <link href="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/venobox/venobox.css')?>" rel="stylesheet">
+  <link href="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/aos/aos.css')?>" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo base_url('/public_wonua/wonuaku/assets/css/style.css')?>" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vesperr - v2.1.0
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="#"><img src="<?php echo base_url('/assets/images/logo_sulsel.png');?>" class="logo_sulsel" style="float:left;margin-right:20px;" />
+        <span style="font-size:20px">BPSDM Prov. Sulsel</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!--<a href="index.html"><img src="<?php echo base_url('/public_wonua/wonuaku/assets/img/logo.png');?>" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="https://bpsdm.sulselprov.go.id/" target="_blank">BPSDM</a></li>
+          <li><a href="<?php echo getenv('base_domain');?>/bpsdm-sekretariat" target="_blank">Sekretariat</a></li>
+          <li><a href="<?php echo getenv('base_domain');?>/bpsdm-admin_bankdata" target="_blank">Bank Data</a></li>
+          <li><a href="<?php echo getenv('base_domain');?>/bpsdm-frontoffice/index.php/Akuntamupegawai/index_pegawai" target="_blank">Akun Pegawai</a></li>
+          <li><a href="<?php echo getenv('base_domain');?>/bpsdm-frontoffice/index.php/Akuntamupegawai" target="_blank">Akun Tamu</a></li>
+          <!--<li><a href="#pricing">Pricing</a></li>-->
+          <li class="drop-down"><a href="">Bidang BPSDM</a>
+            <ul>
+              <li><a href="<?php echo getenv('base_domain');?>/bpsdm-diklatpim" target="_blank">Pendidikan dan Pelatihan Kepemimpinan</a></li>
+              <!--
+              <li class="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>-->
+              <li><a href="<?php echo getenv('base_domain');?>/bpsdm-diklatnis" target="_blank">Pendidikan dan Pelatihan Teknis</a></li>
+              <li><a href="<?php echo getenv('base_domain');?>/bpsdm-rendik" target="_blank">Perencanaan Kediklatan</a></li>
+              <li><a href="<?php echo getenv('base_domain');?>/bpsdm-banoklat" target="_blank">Pengembangan dan Inovasi Kediklatan</a></li>
+              <li><a href="<?php echo getenv('base_domain');?>/bpsdm-upt1" target="_blank">UPT 1 BPSDM</a></li>
+              <li><a href="<?php echo getenv('base_domain');?>/bpsdm-upt2" target="_blank">UPT 2 BPSDM</a></li>
+            </ul>
+          </li>
+          <!--<li><a href="#contact">Contact</a></li>-->
+
+          <li class="get-started"><a href="<?php echo getenv('base_domain');?>/bpsdm-ruangkaban" target="_blank">Ruang Kaban</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h2 data-aos="fade-up"><span style="font-size:40px">Welcome to e-Sinra BPSDM Prov. Sulsel</span></h2>
+          <h4 data-aos="fade-up" data-aos-delay="400">Selamat datang di Kantor Elektronik Badan Pengembangan Sumber Daya Manusia Provinsi Sulawesi Selatan</h4>
+          <div data-aos="fade-up" data-aos-delay="800">
+            <a href="<?php echo getenv('base_domain');?>/bpsdm-frontoffice" class="btn-get-started scrollto" target="_blank">Front Office</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
+          <div class="owl-carousel portfolio-details-carousel">
+            <img src="<?php echo base_url('/assets/images/gubwagub.png');?>" class="img-fluid animated" alt="">
+            <img src="<?php echo base_url('/assets/images/kaban.png');?>" class="img-fluid animated" alt="">
+            <img src="<?php echo base_url('/assets/images/corpu2.jpg');?>" class="img-fluid animated" alt="">
+            <img src="<?php echo base_url('/assets/images/berita172.jpg');?>" class="img-fluid animated" alt="">
+            <img src="<?php echo base_url('/assets/images/berita182.jpg');?>" class="img-fluid animated" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients clients">
+      <div class="container">
+
+        <div class="row" style="display:flex;justify-content:space-around;">
+
+          <div class="col-lg-2 col-md-4 col-6"><!--tiktik <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">bx bx-world-->
+            <div class="icon-box">
+              <div class="icon"><span style="font-size:40px;"><i class="ri-database-2-line" style="color: #47aeff;"></i></span></div>
+              <h5 class="title"><a href="<?php echo getenv('base_domain');?>/bpsdm-admin_bankdata" target="_blank">Bank Data</a></h5>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6"><div class="icon-box">
+              <div class="icon"><span style="font-size:40px;"><i class="ri-gradienter-line" style="color: #ffa76e;"></i></span></div>
+              <h5 class="title"><a href="<?php echo getenv('base_domain');?>/bpsdm-ruangkaban" target="_blank">Ruang Kaban</a></h5>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6"><div class="icon-box">
+              <div class="icon"><span style="font-size:40px;"><i class="ri-bar-chart-box-line" style="color: #5578ff;"></i></span></div>
+              <h5 class="title"><a href="<?php echo getenv('base_domain');?>/bpsdm-sekretariat" target="_blank">Sekretariat</a></h5>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6"><div class="icon-box">
+              <div class="icon"><span style="font-size:40px;"><i class="ri-file-list-3-line" style="color: #11dbcf;"></i></span></div>
+              <h5 class="title"><a href="https://ojs.bpsdmsulsel.id" target="_blank">Journal</a></h5>
+            </div>
+          </div>
+          <!--
+          <div class="col-lg-2 col-md-4 col-6">
+            <img src="<?php echo base_url('/public_wonua/wonuaku/assets/img/clients/client-5.png');?>" class="img-fluid" alt="" >
+          </div>
+
+          <div class="col-lg-2 col-md-4 col-6">
+            <img src="<?php echo base_url('/public_wonua/wonuaku/assets/img/clients/client-6.png');?>" class="img-fluid" alt="" >
+          </div>-->
+
+        </div>
+
+      </div>
+    </section><!-- End Clients Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+        <div class="col-lg-6 text-lg-left text-center">
+          <div class="copyright">
+            &copy; Copyright <strong>BPSDM Provinsi Sulawesi Selatan</strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/vesperr-free-bootstrap-template/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
+            <a href="https://bpsdm.sulselprov.go.id/" target="_blank" class="scrollto">Home Site</a>
+            <a href="#about" class="scrollto">About</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Use</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/jquery/jquery.min.js')?>"></script>
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/jquery.easing/jquery.easing.min.js')?>"></script>
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/php-email-form/validate.js')?>"></script>
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/waypoints/jquery.waypoints.min.js')?>"></script>
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/counterup/counterup.min.js')?>"></script>
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/owl.carousel/owl.carousel.min.js')?>"></script>
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/isotope-layout/isotope.pkgd.min.js')?>"></script>
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/venobox/venobox.min.js')?>"></script>
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/vendor/aos/aos.js')?>"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo base_url('/public_wonua/wonuaku/assets/js/main.js')?>"></script>
+
+</body>
+
+</html>
